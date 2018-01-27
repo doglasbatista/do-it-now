10ef4764189a211a26503e26324faabf089a2fb4
doglasbatista <doglasbatista@gmail.com>
2018-01-27 17:23:43 -0200

    Created css class to card

---
+++ src/components/Registration/SignUp/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="sign-up">
+    <div class="sign-up__content">
+      <img class="logo" src="~@/assets/imgs/logo.png">
+      <form class="card sign-up-form">
+        <legend class="sign-up-form__title">Criar Conta</legend>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+
+};
+
+</script>
+
+<style lang="scss" scoped>
+  @import './main.scss';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from '@/components/Home';
+
+import SignUp from '@/components/Registration/SignUp';
+
 
 Vue.use(Router);
 
@@ -10,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp,
     },
   ],
 });
