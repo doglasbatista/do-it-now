4d4757507f981ba62b9955ddeb17ce2ac7a8bd4b
doglasbatista <doglasbatista@gmail.com>
2018-01-27 15:28:49 -0200

    Created home component

---
+++ src/components/Home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="home-section">
+    <div class="home-section__container">
+      <img src="./imgs/img_responsive.png">
+      <img class="logo" src="~@/assets/imgs/logo.png">
+      <div class="navigation-container">
+        <router-link class="btn-primary" to="/foo" tag="button">Criar Conta</router-link>
+        <router-link class="btn-secondary" to="/foo" tag="button">Entrar</router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+
+</script>
+
+<style lang="scss" scoped>
+ @import './main.scss';
+</style>
+
