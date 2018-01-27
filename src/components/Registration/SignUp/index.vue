d01edc68aa7604a72da19b91820c37b6a08a1866
doglasbatista <doglasbatista@gmail.com>
2018-01-27 17:50:31 -0200

    Created css of sign-up form

--- src/components/Registration/SignUp/index.vue
+++ src/components/Registration/SignUp/index.vue
@@ -4,6 +4,21 @@
       <img class="logo" src="~@/assets/imgs/logo.png">
       <form class="card sign-up-form">
         <legend class="sign-up-form__title">Criar Conta</legend>
+
+        <div class="form-group">
+          <label for="name">Nome</label>
+          <input type="text" class="no-radius-bottom" name="name" id="name" placeholder="Nome">
+        </div>
+
+        <div class="form-group">
+          <label for="email">Endereco de e-mail</label>
+          <input type="email" class="no-radius" name="email" id="email" placeholder="Endereco de e-mail">
+        </div>
+
+        <div class="form-group">
+          <label for="password">Senha</label>
+          <input type="password" class="not-radius-top" name="password" id="password" placeholder="Senha">
+        </div>
       </form>
     </div>
   </section>
