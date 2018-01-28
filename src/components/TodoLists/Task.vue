7dc052f24f6d5372f171b3372cf58fd104ec7605
doglasbatista <doglasbatista@gmail.com>
2018-01-28 13:26:21 -0200

    Basic structure of task

---
+++ src/components/TodoLists/Task.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="task">
+    <legend class="task__title">
+      <input type="checkbox" name="task-name" id="task-name">
+      <label for="task-name">Tarefa 1</label>
+      <button class="destroy-task"></button>
+    </legend>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Task',
+};
+
+</script>
+
+<style lang="scss" scoped>
+  @import './task.scss'
+</style>
--- src/components/TodoLists/Tasks.vue
+++ src/components/TodoLists/Tasks.vue
@@ -2,15 +2,26 @@
   <section class="tasks">
     <div class="form-group">
       <label for="new-task">Adicionar tarefa</label>
-      <input type="text" name="new-task" id="new-task" class="new-task" placeholder="Adicionar tarefa">
+      <input
+        type="text"
+        name="new-task"
+        id="new-task"
+        class="new-task"
+        placeholder="Adicionar tarefa"
+      >
     </div>
+    <task />
   </section>
 </template>
 
 <script>
+import Task from './Task';
 
 export default {
   name: 'Tasks',
+  components: {
+    Task,
+  },
 };
 
 </script>
