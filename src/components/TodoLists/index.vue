107f2bcae6fcd059f4394ac38dfb4cc03c2ddc9a
doglasbatista <doglasbatista@gmail.com>
2018-02-04 11:58:51 -0200

    Added application menu on todo lists

--- src/components/TodoLists/index.vue
+++ src/components/TodoLists/index.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="todo-lists">
+    <application-menu />
     <div class="todo-lists-content">
       <legend class="todo-lists__title">Listas</legend>
       <button class="add-item"></button>
@@ -9,12 +10,15 @@
 </template>
 
 <script>
+import ApplicationMenu from '@/components/shared/ApplicationMenu';
+
 import TodoList from './List';
 
 export default {
   name: 'TodoLists',
   components: {
     TodoList,
+    ApplicationMenu,
   },
 };
 
